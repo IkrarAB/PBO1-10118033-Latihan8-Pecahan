a1e32e4dbf889486810cde8adb4cd7d54a21b670
IkrarAB <ikraranugrah26@gmail.com>
2019-09-23 23:04:43 +0700

    Initial commit

---
+++ src/pbo1/pkg10118033/latihan8/PBO110118033Latihan8.java
@@ -0,0 +1,21 @@
+package pbo1.pkg10118033.latihan8;
+
+/*
+ *
+ * @author
+ * NAMA  : Ikrar Anugrah Bastary
+ * KELAS : PBO1
+ * NIM   : 10118033
+ *
+ */
+
+public class PBO110118033Latihan8 {
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float  : " +f);
+        System.out.println("Double : " +d);
+    }
+}
